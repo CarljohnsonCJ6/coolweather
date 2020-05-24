4927deee4685188ceead91867c597c71db9efd5b
CJ <996234533@qq.com>
2020-05-24 15:59:11 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -177,7 +179,10 @@ public class WeatherActivity extends AppCompatActivity {
             loadBingPic();
         }
 
-        private void showWeatherInfo(Weather weather){
+    /**
+     * 处理并展示Weather实体类中的数据
+     */
+    private void showWeatherInfo(Weather weather){
              String cityName=weather.basic.cityName;
              String updateTime=weather.basic.update.updateTime.split(" ")[1];
              String degree=weather.now.temperature+"℃";
@@ -210,6 +215,8 @@ public class WeatherActivity extends AppCompatActivity {
              carWashText.setText(carWash);
              sportText.setText(sport);
              weatherLayout.setVisibility(View.VISIBLE);
+             Intent intent=new Intent(this, AutoUpdateService.class);
+             startService(intent);
          }
 
     /**
